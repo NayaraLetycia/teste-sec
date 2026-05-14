31feb1dae7b0dba2199aa5402929d318f30dd530
Nayara <131922362+NayaraLetycia@users.noreply.github.com>
2026-05-14 15:23:30 -0300

    Update sec-senha.py

--- code/sec-senha.py
+++ code/sec-senha.py
@@ -1,9 +1,9 @@
 import os
 
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY")
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 
 def start_session():
-    if not AWS_ACCESS_KEY_ID:
+    if not AWS_ACCESS_KEY:
         return "Erro: Chave não configurada!"
     return "Chave configurada com sucesso"
 print(start_session())
