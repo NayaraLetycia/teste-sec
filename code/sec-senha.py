ca05f51705e2bb89f3642fc9b30d9982b706768e
Nayara <131922362+NayaraLetycia@users.noreply.github.com>
2026-05-14 14:24:25 -0300

    Update sec-senha.py

--- code/sec-senha.py
+++ code/sec-senha.py
@@ -1,6 +1,7 @@
 # Simulando uma chave de API real (padrão que scanners detectam fácil)
-AWS_SECRET_KEY = "AKIAIMRE7GV76B7D5FWA" 
-GITHUB_TOKEN = "ghp_n0tArEalT0kEnThAtL00ksREal1234567890"
+#AWS_SECRET_KEY = "AKIAIMRE7GV76B7D5FWA" 
+#GITHUB_TOKEN = "ghp_n0tArEalT0kEnThAtL00ksREal1234567890"
 
 def connect_service():
-    print(f"Conectando com a chave: {AWS_SECRET_KEY}")
+    #print(f"Conectando com a chave: {AWS_SECRET_KEY}")
+    print(f"Conectando com a chave: {AWS_ACCESS_KEY}")
