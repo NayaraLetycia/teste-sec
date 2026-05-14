60f45247ed5e98e0994ffba9dc53b38d98c377ed
Nayara <131922362+NayaraLetycia@users.noreply.github.com>
2026-05-14 17:36:56 -0300

    Update sec-senha.py

--- code/sec-senha.py
+++ code/sec-senha.py
@@ -7,3 +7,13 @@ def start_session():
         return "Erro: Chave não configurada!"
     return "Chave configurada com sucesso"
 print(start_session())
+
+import sqlite3
+#se alguém substitui o valor da variável por 1 or 1=1, essa query pode retornar todos os dados
+
+def busca_usuario(user_id):
+    query = "SELECT * FROM usuarios WHERE id = " + user_id
+    
+    print(f"Executando query: {query}")
+    return "Dados do usuário retornados"
+busca_usuario("10")
