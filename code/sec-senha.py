ce1dbaf0a129da86586c342908e35bfcff719f18
Nayara <131922362+NayaraLetycia@users.noreply.github.com>
2026-05-14 17:52:47 -0300

    Update sec-senha.py

--- code/sec-senha.py
+++ code/sec-senha.py
@@ -8,6 +8,8 @@ def start_session():
     return "Chave configurada com sucesso"
 print(start_session())
 
+############################################## 1
+
 import sqlite3
 #se alguém substitui o valor da variável por 1 or 1=1, essa query pode retornar todos os dados
 
@@ -17,3 +19,19 @@ def busca_usuario(user_id):
     print(f"Executando query: {query}")
     return "Dados do usuário retornados"
 busca_usuario("10")
+
+################################################ 2
+import os
+
+def ping_host(hostname):
+    # VULNERÁVEL: Se hostname for "google.com ; rm -rf /", o comando deleta tudo
+    os.system("ping -c 1 " + hostname)
+
+ping_host("8.8.8.8")
+
+################################################ 3 
+import pickle
+
+def carregar_configuracao(dados_binarios):
+    # VULNERÁVEL: Pode executar código arbitrário ao carregar. Melhor escolher um JSON, que só transportam dados
+    return pickle.loads(dados_binarios)
